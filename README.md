c63eda9dbd654643bc49598334d5ca246433a960
demget <30910794+demget@users.noreply.github.com>
2024-01-09 21:01:36 +0000

    passgen: implement rand gen by a given length

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 ## TODO
 
-1. Research, think about, describe the possible ways to generate a password.
+1. Research, think about, describe the possible ways to generate a password.    
 2. Set up the project locally using Goland, Git.
 3. Implement a basic nubmer password generation (your input is a password length, your output is a random number withing this length)
--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
 
 func main() {
 	var length int
 
-	fmt.Print("Password length: ")
+	fmt.Print("Password length : ")
 	_, err := fmt.Scanln(&length)
 	if err != nil {
 		fmt.Println("Password length MUST be a number!")
 		return
 	}
-
-	fmt.Println(length)
+	var numbers = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	password := ""
+	for i := 0; i < length; i++ {
+		randVal := numbers[rand.Intn(len(numbers))]
+		password += strconv.Itoa(randVal)
+	}
+	fmt.Println("Yes")
+	fmt.Println(password)
 }
