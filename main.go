931b9ce26364973be4600150b63b555acd950a4a
Apolisk <a.bogdanovitch8@gmail.com>
2024-01-12 17:32:15 +0200

    passgen: implement generate func

--- main.go
+++ main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"os"
 	"fmt"
 	"math/rand"
+	"os"
 	"strconv"
 )
 
 func main() {
 	var length int
+	var uppercase bool
+
 	input(&length, "Password length:", "Password length MUST be a number!")
+	input(&uppercase, "Type 1  if you want to use uppercase character, else type 0", "Must be bool type")
 
-	var password string
-	for i := 0; i < length; i++ {
-		password += strconv.Itoa(rand.Intn(10))
-	}
-
-	fmt.Println(password)
+	fmt.Println(generate(length, uppercase))
 }
 
 func input(v any, m, e string) {
@@ -26,4 +24,25 @@ func input(v any, m, e string) {
 		fmt.Printf("\n%s (%s)\n", e, err)
 		os.Exit(1)
 	}
+}
+
+func generate(l int, u bool) string {
+	var password string
+	const UpperLetters = "ABCDEFGHIKLMNOPQRSTVXYZ"
+
+	if l <= 0 {
+		return "Password length must  be greater then 0"
+	} else if l > 0 && u == false {
+		for i := 0; i < l; i++ {
+			password += strconv.Itoa(rand.Intn(10))
+		}
+	} else {
+		for i := 0; i < l; i++ {
+			randnum := strconv.Itoa(rand.Intn(10))
+			randchar := string(rune(UpperLetters[rand.Intn(len(UpperLetters))]))
+			password += randnum
+			password += randchar
+		}
+	}
+	return password
 }
